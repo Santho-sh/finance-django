3f328d5b7ebbf1feab6b5213aa3e4fead3b75d18
Santhosh <santhoshkalish333@gmail.com>
2023-02-11 01:15:39 +0530

    login not working

--- finance_app/finance/forms.py
+++ finance_app/finance/forms.py
@@ -5,7 +5,4 @@ from django.contrib.auth.forms import UserCreationForm
 class RegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'password1', 'password2', )
-
-    def save(self, commit= True):
-        return super(RegisterForm, self).save(commit)
+        fields = ('username', 'password1', 'password2')
--- finance_app/finance/views.py
+++ finance_app/finance/views.py
@@ -1,29 +1,57 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate, logout
 from .forms import RegisterForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from .models import Person, Stocks
 
 
+
 def index(response):
     
-    if response.method == 'POST':
-        stocks = Stocks.objects.all()
-        return render(response, 'finance/index.html', {'stocks':stocks})
-    else:
-        return redirect('/login')
+    stocks = Stocks.objects.all()
+    return render(response, 'finance/index.html', {'stocks':stocks})
 
 def register(response):
+    
     if response.method == 'POST':
         form = RegisterForm(response.POST)
-        print(form)
         if form.is_valid():
             user = form.save()
             login(response, user)
             messages.success(response, "Registration successful.")
             return redirect('finance:home')
         
-        messages.error(response, "Unsuccessful registration. Invalid information.")
-    
-    form = RegisterForm()
+        else:
+            messages.error(response, "Unsuccessful registration. Invalid information.")
+            return redirect('/login')
+    else:
+        form = RegisterForm()
     return render(response, 'finance/register.html', {'form': form})
+
+
+def login_request(response):
+    
+    if response.method == 'POST':
+        form = AuthenticationForm(response.POST)
+        if form.is_valid():
+            username = form.cleaned_data('username')
+            password = form.changed_data('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(response, user)
+                messages.info(response, f"You are now logged in as {username}.")
+                return redirect('finance:home')
+            else:
+                messages.error(response,"Invalid username or password.")
+        else:    
+            messages.error(response,"Invalid username or password.")
+    else:
+        form = AuthenticationForm()    
+        return render(response, 'registration/login.html', {'form':form})
+
+
+def logout_request(response):
+    logout(response)
+    messages.info(response, "You have successfully logged out.")
+    return redirect("finance:homepage")
